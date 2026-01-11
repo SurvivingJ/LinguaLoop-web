8b7f45145fcd72bec1b661a0ccf64d0aacbe7678
SurvivingJ <jamesccmcb@gmail.com>
2026-01-11 14:25:35 +1100

    wsgi file change

--- wsgi.py
+++ wsgi.py
@@ -10,5 +10,5 @@ app = create_app()
 if __name__ == "__main__":
     # This allows running the WSGI file directly for testing
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
